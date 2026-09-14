e61860dbca7258ff4faa2e49193a2aa9913b4fc2
Zahraa Al-Sharifeh <zahraasharifeh2003@gmail.com>
2026-09-14 14:20:33 +0300

    Add personal portfolio site

--- src/data/site.ts
+++
@@ -1,9 +0,0 @@
-export const site = {
-  name: 'Zahraa Sharifeh',
-  title: 'Full-Stack Engineer',
-  location: 'Beirut',
-  email: 'zahraasharifeh2003@gmail.com',
-  linkedin: 'https://www.linkedin.com/in/zahraa-sharifeh',
-  github: 'https://github.com/zahraa-sharifeh',
-  resumePdf: '/Zahraa_Sharifeh_Resume.pdf',
-} as const
